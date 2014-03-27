aac21555b92d7c98bba146782aa9282d809895e8
glz <glz@mail.com>
2014-03-27 12:21:29 +0800

    instance-methods

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ module.exports = Class = function(argv) {
     return function(){};
   
   var constructor = argv['initialize'];
+  for (var method in argv) {
+    if (argv.hasOwnProperty(method) && method !== 'initialize')
+      constructor.prototype[method] = argv[method];
+  }
   return constructor;
 }
 
